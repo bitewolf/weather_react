77096c29ef81ab08e71cff8d20f3c74f7e2758eb
bitewolf <2189108@gmail.com>
2020-12-23 05:56:32 +0300

    fix_time

--- src/App.js
+++ src/App.js
@@ -15,7 +15,9 @@ class App extends React.Component {
     sunrise: undefined,
     sunset: undefined,
     error:  undefined,
-    weather: undefined
+    weather: undefined,
+    time: undefined,
+    timezone: undefined
   }
 
   gettingWeather = async (e) => {
@@ -27,11 +29,7 @@ class App extends React.Component {
       const data = await api_url.json();
       console.log(data);
       console.log(data.weather[0].description);
-
-      
-      let timeSunrise = new Intl.DateTimeFormat('ru-RU', {hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(data.sys.sunrise*1000);
-      let timeSunset = new Intl.DateTimeFormat('ru-RU', {hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(data.sys.sunset*1000);
-
+           
       let pressure = data.main.pressure;
       let pressureInMmHg = Math.floor(pressure * 0.75006);
 
@@ -39,6 +37,13 @@ class App extends React.Component {
       weatherWithoutSpace = weatherWithoutSpace.replace(/ +/g, '').trim();
       console.log(weatherWithoutSpace);
 
+      const newLocal = new Date();
+      let timeLocal = newLocal;
+      let timeNow = timeLocal.getHours() + timeLocal.getTimezoneOffset()/60 + data.timezone/3600;
+      
+      let timeSunrise = new Intl.DateTimeFormat('ru-RU', {hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(data.sys.sunrise*1000 + timeLocal.getTimezoneOffset()*60000 + data.timezone*1000);
+      let timeSunset = new Intl.DateTimeFormat('ru-RU', {hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(data.sys.sunset*1000 + timeLocal.getTimezoneOffset()*60000 + data.timezone*1000);
+      
       this.setState({
         temp: data.main.temp,
         city: data.name,
@@ -47,8 +52,8 @@ class App extends React.Component {
         sunrise: timeSunrise,
         sunset: timeSunset,
         error: undefined,
-        weather: weatherWithoutSpace
-
+        weather: weatherWithoutSpace,
+        time: timeNow
       });
     } else {
       this.setState({
@@ -59,7 +64,8 @@ class App extends React.Component {
         sunrise: undefined,
         sunset: undefined,
         weather: undefined,
-        error: "Введите название города"
+        error: "Введите название города",
+        time: undefined
       });
     }
   };
@@ -72,13 +78,21 @@ class App extends React.Component {
     {
     classWeather += ' '+this.state.weather;
     };
-    let time = new Date();
-    if (time.getHours() > 23 && time.getHours() < 6)
-    {
-      classWeather += ' night';
-    } else 
-      classWeather += ' day';
-    ;
+    if (this.state.time) {
+      let time = this.state.time;
+      if (time < 0) {
+        time += 24;
+      }
+       if (time > 23 || time < 6)
+      {
+        classWeather += ' night';
+      } else {
+          classWeather += ' day';
+      }
+    }
+
+
+    
 
     return (
       <div className="wrapper">
