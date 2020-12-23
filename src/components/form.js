b2b759d6be112c975e760a702479cbe43f53ea32
bitewolf <2189108@gmail.com>
2020-12-23 23:17:37 +0300

    фикс

--- src/components/form.js
+++ src/components/form.js
@@ -3,8 +3,8 @@ import React from "react";
 const Form = props => (
 <div>
     <form onSubmit={props.weatherMethod}>
-        <input type="text" name="city" placeholder="Город"/>
-        <button>Получить инфу о погоде</button>
+        <input type="text" name="city" placeholder="Введите город"/>
+        <button>Искать</button>
     </form>
 </div>
 )
--- src/components/info.js
+++ src/components/info.js
@@ -1,10 +1,13 @@
 import React from "react";
 
 const Info = props => (
+    <div className="infoText">
+    { props.temp && 
     <div className="infoText"> 
         <p className="time">{props.time}</p>
         <p className="temp">{props.temp} °C</p>
         <p className="tempFeels">Ощущается как: {props.tempFeels} °C</p>
+    </div>}
     </div>
 );
 
